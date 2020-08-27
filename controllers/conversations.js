69074a6fc7d18e21d54dc41e526f67e10cdd3746
Matt Seidholz <mseidholz@gmail.com>
2020-08-27 14:15:06 -0400

    figured out how to write file content to the dom

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -1,14 +1,52 @@
 const fs = require('fs');
 
 const get_conversations = (req, res) =>{
+
     let conversations = []
     fs.readdir('./conversations', (err,files) => {
         files.map(file => {
-            conversations.push({"id":file})
+            return conversations.push({"id":file})
         })
+
+    // sure there's a more efficient way of doing this...
+    conversations.map((c) => {
+        c.content = fs.readFileSync(`./conversations/${c.id}`, 'utf8', (err, data) => { 
+            console.log(data)   
+            return data;
+        })
+    })
+
     return res.status(200).send(conversations)
     })
 }
+
+
+// SOME SCRATCH,
+// TRYING TO READ CONTENT OF EACH FILE
+// USE REDUCE() YOU COWARD
+// conversationProcesser = (file, content, arr) => {
+//     arr.push({"id":file, "content":content})
+//     console.log(arr)
+//     return arr;
+// }
+
+
+// fs.readdir('./conversations', (err,files) => {
+//     files.map(file => {
+//         console.log(file)
+//         conversations.push({"id" : file})
+//         console.log(conversations)
+//         return conversations
+        
+//         // fs.readFile(`./conversations/${file}`, "utf8", (err,data) =>{
+//         //     if (err) throw err;
+//         //     const content = data;
+//         //     console.log(content)
+//         //     return conversationProcesser(file, content, conversations);
+//         // })
+//     })
+
+//     return conversations
 
 
 const delete_conversations = (req, res) =>{
