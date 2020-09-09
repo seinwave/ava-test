d2ed4b1b968c16442c73212470f7712377355d47
Matt Seidholz <mseidholz@gmail.com>
2020-09-09 11:00:33 -0400

    used equal signs instead of colons in mongoose model. Whoopsie.

--- models/MongoConversations.js
+++ models/MongoConversations.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const conversationSchema = new Schema({
-   fileName = String,
-   id = String,
-   lastMutation = String,
-   content = String,
+   fileName: String,
+   id: String,
+   lastMutation: String,
+   content: String,
 });
 
 mongoose.model('conversations', conversationSchema, 'conversations')
