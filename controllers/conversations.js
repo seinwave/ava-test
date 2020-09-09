6ee459b4ebf5096744d236b13cb21fce825d824d
Matt Seidholz <mseidholz@gmail.com>
2020-09-09 11:21:00 -0400

    tying to get convos from MongoDB

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -19,7 +19,7 @@ const get_conversations = (req, res) =>{
     let content
     let lastMutation = [];
     let Convos = MongoConversation.find({})
-    console.log(Convos.collection.collection);
+    console.log(Convos.collection);
 
     fs.readdir('./conversations', (err,files) => {
         files.map(file => {
